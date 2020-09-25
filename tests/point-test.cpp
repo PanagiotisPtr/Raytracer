6897903284260a8b14368c2cb5db2c66123f444c
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2020-09-25 21:19:58 +0300

    Added point tests

--- tests/point-test.cpp
+++ tests/point-test.cpp
@@ -1,7 +1,31 @@
 #include "gtest/gtest.h"
 #include "math/point.h"
 
-TEST(pointTests, constructorTest1) {
+#include <cstddef>
+
+TEST(pointTests, constructorPositiveValues) {
     math::Point<float, 5> t = {2,3,4,5,6};
-    EXPECT_EQ (true, true);
+    for (std::size_t i = 2, j = 0; i <= 6 && j < 5; i++, j++) {
+        EXPECT_EQ(i, t[j]);
+    }
+}
+
+TEST(pointTests, constructorNegativeValues) {
+    math::Point<float, 5> t = {-2,-3,-4,-5,-6};
+    for (int i = 2, j = 0; i <= 6 && j < 5; i++, j++) {
+        EXPECT_EQ(-i, t[j]);
+    }
+}
+
+TEST(pointTests, constructorSizeTest) {
+    math::Point<float, 10> t;
+    EXPECT_EQ(10, t.size());
+}
+
+TEST(pointTests, constructorTypesTest) {
+    EXPECT_EQ(typeid(float), typeid(math::Point<float, 1>::value_type));
+    EXPECT_EQ(typeid(int), typeid(math::Point<int, 1>::value_type));
+    EXPECT_EQ(typeid(std::size_t), typeid(math::Point<std::size_t, 1>::value_type));
+    EXPECT_EQ(typeid(unsigned), typeid(math::Point<unsigned, 1>::value_type));
+    EXPECT_EQ(typeid(long), typeid(math::Point<long, 1>::value_type));
 }
