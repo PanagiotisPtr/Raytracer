2d64f0a93480f7d3a4bcb1255078190dbef1a4e8
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2020-10-17 17:31:55 +0300

    A sphere is now required to have a material

--- src/objects/sphere.h
+++ src/objects/sphere.h
@@ -3,6 +3,8 @@
 
 #include "object.h"
 #include "primitives/constants.h"
+#include "primitives/material.h"
+#include "primitives/base_material.h"
 
 namespace objects {
 
@@ -10,11 +12,17 @@ class Sphere : public Object {
 public:
     // Spheres have radius 1
     // To change the radius they need transformations
-    Sphere(primitives::Point3D o) : origin(o) {}
+    Sphere(primitives::Point3D o)
+    : origin(o) {}
 
     primitives::Point3D getOrigin() const { return this->origin; }
+
+    primitives::BaseMaterial getMaterial() const { return this->material; }
+
+    void setMaterial(primitives::BaseMaterial m) { this->material = m; }
 private:
     primitives::Point3D origin;
+    primitives::BaseMaterial material;
 };
 
 } // namespace objects
