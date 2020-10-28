bee1f078cc383922cd9dbb7cc00388de6792915d
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2020-10-28 20:49:29 +0200

    Used template metaprogramming to make specialisation more specific for colour operators

--- src/drawing/operators.h
+++ src/drawing/operators.h
@@ -1,6 +1,9 @@
 #ifndef DRAWING_OPERATORS_H
 #define DRAWING_OPERATORS_H
 
+#include <type_traits>
+
+#include "colour.h"
 #include "operations.h"
 
 namespace drawing {
@@ -17,17 +20,17 @@ Colour operator*(const Colour& c1, const Colour& c2) {
     return Operations::multiply(c1, c2);
 }
 
-template<typename T>
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, int> = 0>
 Colour operator*(T s, const Colour& c) {
     return Operations::multiply(s, c);
 }
 
-template<typename T>
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, int> = 0>
 Colour operator*(const Colour& c, T s) {
     return Operations::multiply(s, c);
 }
 
-template<typename T>
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, int> = 0>
 Colour operator/(const Colour& c, T s) {
     return Operations::divide(s, c);
 }
