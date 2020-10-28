cc6b4e8a03f8b53d0105122088ed04117570fdb5
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2020-10-28 20:47:49 +0200

    Removed the use of colour overloaded operators and replaced them with operations class calls. Also, added function to calculate the camera transformation matrix (actually this is used to transform the world not the camera but it can be thought of this way)

--- src/world/operations.h
+++ src/world/operations.h
@@ -3,14 +3,14 @@
 
 #include <cmath>
 
-#include "../drawing/colour.h"
-#include "../drawing/operators.h"
-#include "../primitives/ray.h"
-#include "../primitives/intersection_container.h"
-#include "../primitives/constants.h"
-#include "../primitives/operations.h"
-#include "../objects/light.h"
-#include "../math/operations.h"
+#include "drawing/colour.h"
+#include "primitives/ray.h"
+#include "primitives/intersection_container.h"
+#include "primitives/constants.h"
+#include "primitives/operations.h"
+#include "objects/light.h"
+#include "math/operations.h"
+#include "math/transformations.h"
 #include "world.h"
 
 namespace world {
@@ -38,13 +38,63 @@ public:
                 drawing::Colour colour =
                     primitives::Operations::getColourOnSphere(object.getMaterial(), l, p, in, normal);
 
-                finalColour = finalColour + colour;
+                finalColour = drawing::Operations::add(finalColour, colour);
             }
         } catch(const primitives::NoIntersectionsException& e) {
             return finalColour;
         }
 
         return finalColour;
+    }
+
+    // This actually calculates the transformation the world needs
+    // so that it looks like the camera moved.
+    static primitives::TransformationMatrix calculateCameraTransformation(
+        primitives::Point3D position,
+        primitives::Point3D target,
+        primitives::Vector3D up
+    ) {
+        // need to compute new
+        // standard basis vectors
+        primitives::TransformationMatrix t = primitives::TransformationMatrix::getIdentity();
+        
+        primitives::Vector3D front = math::Operations::normalise(target - position);
+
+        math::Vector<primitives::PrecisionType, 3> front3;
+        front3[0] = front[0];
+        front3[1] = front[1];
+        front3[2] = front[2];
+
+        math::Vector<primitives::PrecisionType, 3> up3;
+        up3[0] = up[0];
+        up3[1] = up[1];
+        up3[2] = up[2];
+
+        primitives::Vector3D left;
+        math::Vector<primitives::PrecisionType, 3> left3 = math::Operations::crossProduct3D(
+            front3,
+            math::Operations::normalise(up3)
+        );
+        left[0] = left3[0];
+        left[1] = left3[1];
+        left[2] = left3[2];
+        left[3] = 0;
+
+        primitives::Vector3D orthogonalUp;
+        math::Vector<primitives::PrecisionType, 3> orthogonalUp3 = math::Operations::crossProduct3D(
+            left3,
+            front3
+        );
+        orthogonalUp[0] = orthogonalUp3[0];
+        orthogonalUp[1] = orthogonalUp3[1];
+        orthogonalUp[2] = orthogonalUp3[2];
+        orthogonalUp[3] = 0;
+
+        t[0] = left;
+        t[1] = orthogonalUp;
+        t[2] = primitives::Vector3D({0,0,0,0}) - front;
+
+        return t * math::Transformations::translate(-position[0], -position[1], -position[2]);
     }
 };
 
