b6f2282e00d832b63accbaee781070e5996abf86
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2020-11-14 15:02:21 +0200

    The reduntant data from the coverage report is now removed in another command. Also, now using the bash script instead of long commands to generate coverage report in the ci/cd pipeline.

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,5 +1,6 @@
-lcov --exclude "/Applications/Xcode.app/*" \
---exclude "`pwd`/lib/*" \
---base-directory build/tests/CMakeFiles/raytracer_tests.dir \
---capture --directory build/tests/CMakeFiles/raytracer_tests.dir --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
+lcov --capture --directory build/tests/CMakeFiles/raytracer_tests.dir --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
+lcov --remove build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info -o build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info \
+"/Applications/Xcode.app/*" \
+"`pwd`/lib/*" \
+"usr/include/*"
 genhtml build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info --output-directory test-coverage
