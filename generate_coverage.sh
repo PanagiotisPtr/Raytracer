277a4c49fb81b4941fe596130ed9a314dff931d7
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2020-11-14 15:07:05 +0200

    Added base directory to shorten file paths in coverage report

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,4 +1,4 @@
-lcov --capture --directory build/tests/CMakeFiles/raytracer_tests.dir --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
+lcov --capture --base-directory build/tests/CMakeFiles/raytracer_tests.dir --directory build/tests/CMakeFiles/raytracer_tests.dir --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
 lcov --remove build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info -o build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info \
 "/Applications/Xcode.app/*" \
 "`pwd`/lib/*" \
