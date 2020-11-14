d9e66d9067309aed8b4ecd27030e2dece5baefe6
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2020-11-14 15:10:48 +0200

    Last fix didn't resolve the std include issue. Trying to ignore that directory specifically

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -2,5 +2,6 @@ lcov --capture --base-directory build/tests/CMakeFiles/raytracer_tests.dir --dir
 lcov --remove build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info -o build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info \
 "/Applications/Xcode.app/*" \
 "`pwd`/lib/*" \
-"usr/include/*"
+"usr/include/*" \
+"6/*"
 genhtml build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info --output-directory test-coverage
