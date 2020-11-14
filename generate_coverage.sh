699ae20f12837079017369bc76e0950f394404aa
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2020-11-14 15:19:18 +0200

    Simplified script to generate coverage reports

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,7 +1,3 @@
-lcov --capture --base-directory build/tests/CMakeFiles/raytracer_tests.dir --directory build/tests/CMakeFiles/raytracer_tests.dir --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
-lcov --remove build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info -o build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info \
-"/Applications/Xcode.app/*" \
-"`pwd`/lib/*" \
-"usr/include/*" \
-"6/*"
+lcov --capture --base-directory . --directory build/tests/CMakeFiles/raytracer_tests.dir --no-external --output-file build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info
+lcov --remove build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info -o build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info "`pwd`/lib/*"
 genhtml build/tests/CMakeFiles/raytracer_tests.dir/raytracer_test_coverage.info --output-directory test-coverage
