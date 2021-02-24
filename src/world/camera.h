41ef260072f077583c74c26288d7a147e709e90f
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2021-02-24 23:23:04 +0200

    Added concurrency using a thread pool with a work queue (4 times faster on a 4 core machine).

--- src/world/camera.h
+++ src/world/camera.h
@@ -2,6 +2,9 @@
 #define WORLD_CAMERA_H
 
 #include <cmath>
+#include <thread>
+#include <mutex>
+#include <queue>
 
 #include "drawing/canvas.h"
 #include "primitives/constants.h"
@@ -15,6 +18,16 @@ namespace world {
 
 template<std::size_t Width, std::size_t Height>
 class Camera {
+private:
+    struct RenderBlock {
+        RenderBlock(unsigned x1, unsigned y1, unsigned x2, unsigned y2)
+        : start({x1, y1}), end({x2, y2}) {}
+
+        RenderBlock() : start({0,0}), end({0,0}) {}
+
+        math::Point<unsigned, 2> start; // top left
+        math::Point<unsigned, 2> end; // bottom right
+    };
 public:
     Camera() : frameHeight(Height), frameWidth(Width) {
         this->transform = primitives::TransformationMatrix::getIdentity();
@@ -75,15 +88,22 @@ public:
         return primitives::Ray(origin, direction);
     }
 
-    drawing::Canvas<Width, Height>* render(const World& w) {
+    drawing::Canvas<Width, Height>* render(const World& w, unsigned blockSize = 64) {
         auto image = new drawing::Canvas<Width, Height>();
+        const unsigned numberOfThreads = std::thread::hardware_concurrency();
 
-        for (std::size_t y = 0; y < Height; y++) {
-            for (std::size_t x = 0; x < Width; x++) {
-                primitives::Ray r = this->getRayForPosition(x, y);
-                drawing::Colour c = Operations::colourAtIntersection(w, r);
-                (*image)[y][x] = c;
-            }
+        this->renderQueue = this->generateRenderBlocks(blockSize);
+
+        std::queue<std::thread> pool;
+        for (unsigned i = 0; i < numberOfThreads; i++) {
+            pool.emplace([this](drawing::Canvas<Width, Height>* image, const World& w) {
+                this->renderProcedure(image, std::cref(w));
+            }, image, std::cref(w));
+        }
+
+        while (!pool.empty()) {
+            pool.front().join();
+            pool.pop();
         }
 
         return image;
@@ -97,6 +117,8 @@ private:
     primitives::PrecisionType frameWidth;
     primitives::PrecisionType fieldOfView;
     primitives::TransformationMatrix transform;
+    std::mutex renderQueueMutex;
+    std::queue<RenderBlock> renderQueue;
 
     primitives::PrecisionType getViewWidth() {
         primitives::PrecisionType aspectRatio = this->getAspectRatio();
@@ -122,6 +144,55 @@ private:
 
     primitives::PrecisionType getPixelSize() {
         return this->getViewWidth() / this->frameWidth;
+    }
+
+    void renderProcedure(drawing::Canvas<Width, Height>* image, const World& w) {
+        bool finished = false;
+        RenderBlock block;
+
+        do {
+            std::unique_lock<std::mutex> guard(this->renderQueueMutex);
+            if (this->renderQueue.empty()) {
+                finished = true;
+            } else {
+                block = this->renderQueue.front();
+                this->renderQueue.pop();
+            }
+            guard.unlock();
+
+            if (! finished) {
+                for (std::size_t y = block.start[1]; y < block.end[1]; y++) {
+                    for (std::size_t x = block.start[0]; x < block.end[0]; x++) {
+                        primitives::Ray r = this->getRayForPosition(x, y);
+                        drawing::Colour c = Operations::colourAtIntersection(w, r);
+                        // the image doesn't need locking because every thread writes
+                        // ina different memory location
+                        (*image)[y][x] = c;
+                    }
+                }
+            }
+        } while (!finished);
+    }
+
+    std::queue<RenderBlock> generateRenderBlocks(unsigned blockSize = 64) {
+        const unsigned verticalBlocks = (Height + blockSize - 1) / blockSize;
+        const unsigned horizontalBlocks = (Width + blockSize - 1) / blockSize;
+        std::queue<RenderBlock> blocks;
+
+        for (unsigned i = 0; i < verticalBlocks; i++) {
+            for (unsigned j = 0; j < horizontalBlocks; j++) {
+                // top left
+                unsigned x1 = j * blockSize;
+                unsigned y1 = i * blockSize;
+                // bottom right
+                unsigned x2 = (j+1) * blockSize > Width ? Width : (j+1) * blockSize;
+                unsigned y2 = (i+1) * blockSize > Height ? Height : (i+1) * blockSize;
+
+                blocks.emplace(x1, y1, x2, y2);
+            }
+        }
+
+        return blocks;
     }
 };
 
