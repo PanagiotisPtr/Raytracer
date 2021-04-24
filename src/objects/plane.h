c72a9e91527091874439c60efa3b0d26109e9725
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2021-04-24 22:13:02 +0100

    Simplified plane logic. Now it's just a very thin cube

--- src/objects/plane.h
+++ src/objects/plane.h
@@ -6,29 +6,23 @@
 #include <math.h>
 
 #include "object.h"
+#include "cube.h"
 #include "math/constants.h"
 
 namespace objects {
 
-class Plane : public Object {
+class Plane : public Cube {
 public:
-    Plane() : Object() {}
-
-    Plane(primitives::Point3D o) : Object(o) {}
-protected:
-    primitives::Vector3D getLocalNormalAt(const primitives::Point3D& p) const override {
-        // plane lies on the axis z and x so normal is always on y pointing up
-        return primitives::Vector3D({0,1,0,0});
+    Plane() : Cube() {
+        this->addTransformation(
+            math::Transformations::scale<primitives::PrecisionType>(1.0, 0.001, 1.0)
+        );
     }
 
-    std::vector<primitives::PrecisionType> getLocalRayIntersections(const primitives::Ray& r) const override {
-        // line is parallel to the plane (also avoid division by 0 later on)
-        if (std::abs(r.getDirection()[1]) <= math::EQUALITY_DELTA) {
-            return {};
-        }
-        // plane's y coordinate is 0.
-        // y1 = y0 + ty => 0 = y0 + ty => y = -y0/y
-        return { -r.getOrigin()[1]/r.getDirection()[1] };
+    Plane(primitives::Point3D o) : Cube(o) {
+        this->addTransformation(
+            math::Transformations::scale<primitives::PrecisionType>(1.0, 0.001, 1.0)
+        );
     }
 };
 
